4d91bf7e689edab2f7e014c7b9d839ee6b076307
Nickolay Marchenko <n30n1ck@gmail.com>
2016-01-12 19:20:59 +0300

    0.18.0

---
+++ build/js/map/rezid.js
@@ -0,0 +1,72 @@
+$(function () {
+    var $map = $('#map');
+
+    if ($map.length) {
+        $map.data({
+            name: 'СИНЕМА',
+            api: '',
+            radius: [0.05, 0.05],
+            mainPoint: {"position": [45.016155, 39.03488], "icon": "rezidentsiya"},
+            categories: [
+                {
+                    "type": "category",
+                    "name": "Больницы и поликлиники",
+                    "keyword": "медицинские учреждения",
+                    "icon": "hospital-building"
+                },
+                {
+                    "type": "category",
+                    "name": "Магазины",
+                    "keyword": "продуктовыt магазины",
+                    "icon": "market"
+                },
+                {
+                    "type": "category",
+                    "name": "Садики и школы",
+                    "keyword": "общеобразовательные учреждения",
+                    "icon": "school"
+                },
+                {
+                    "type": "point",
+                    "name": "Культовый кинотеатр",
+                    "icon": "cinema",
+                    "position": [45.011155, 39.03888]
+                },
+                {
+                    "type": "point",
+                    "name": "Кубанский Гос. Университет",
+                    "icon": "university",
+                    "position": [45.021155, 39.01588]
+                },
+                {
+                    "type": "polyline",
+                    "name": "Общественный транспорт",
+                    "icon": "public-transport",
+                    "items": [
+                        [[45.019320, 39.028439], [45.016519, 39.041951], [45.023210, 39.043238]],
+                        [[45.016519, 39.041951], [45.012329, 39.037154]]
+                    ],
+                    "options": {
+                        "strokeColor": "#c259b5",
+                        "strokeWidth": 3,
+                        "strokeStyle": "6 3"
+                    }
+                },
+                {
+                    "type": "polyline",
+                    "name": "Подъезды на автомобиле",
+                    "icon": "car-access",
+                    "items": [
+                        [[45.017560, 39.036560], [45.016469, 39.036107], [45.016489, 39.035598]],
+                        [[45.018380, 39.032910], [45.016749, 39.032287], [45.016729, 39.033631]]
+                    ],
+                    "options": {
+                        "strokeColor": "#ef4446",
+                        "strokeWidth": 3,
+                        "strokeStyle": "3 3"
+                    }
+                }
+            ]
+        });
+    }
+});
---
+++ build/js/svg_init_rezid.js
@@ -0,0 +1,106 @@
+$(function() {
+
+	if ($("#liter_cinema_svg") != 0) {
+
+		var cinema = Raphael('liter_cinema_svg', "100%", "100%"), attributes = {
+			'stroke-width' : 0,
+			'stroke-linejoin' : 'round'
+		}, arr = new Array();
+	
+		cinema.setViewBox(0, 0, 1067, 800);
+
+
+		for (var country in pathscinema) {
+			var obj = cinema.path(pathscinema[country].path);
+			
+			obj.attr(attributes);
+			obj.attr({
+				fill : pathscinema[country].color 
+			});
+
+			arr[obj.id] = country;
+			
+			/* добавляем data-атрибуты для path, которые потом будем передавать в запросе */
+			$(obj).data("liter", pathscinema[country].liter);
+			$(obj).data("floor", pathscinema[country].floor);
+
+			obj.hover(function() {
+				$(".floor_label").show();
+				$(".floor_label_num").html(pathscinema[arr[this.id]].floor);
+			}, function() {
+				$(".floor_label").hide();
+			});
+
+			obj.click(function() {
+				$("#response_popup").load( 'get_flats.html' , function (){ 
+					$(".popup_caller").magnificPopup({
+						type : 'inline',
+						preloader : false,
+						removalDelay : 300,
+						mainClass : 'my-mfp-zoom-in',
+						overflowY : "scroll",
+                        close: function() {
+                            $("#response_popup").empty();
+                        }
+					});
+					$(".popup_caller").click();
+				} );
+				
+				//load потому что мы не на сервере, при переносе на сервак лучше бы заменить на это
+				/*$.get('get_flats.php [или какой угодно удобный путь к скрипту, важно лишь то, что он вернет]', $(this).data(), function(innerHTML){
+					$("#response_popup").html(innerHTML);
+					$(".popup_caller").magnificPopup({
+						type : 'inline',
+						preloader : false,
+						removalDelay : 300,
+						mainClass : 'my-mfp-zoom-in',
+						overflowY : "scroll",
+                        close: function() {
+                            $("#response_popup").empty();
+                        }
+					});
+					$(".popup_caller").click();
+				});*/
+			});
+
+		}
+
+		if ($(".mob_floor_selector") != 0) {
+
+			$(".mob_floor_selector__item").click(function() {
+				$("#response_popup").load( 'get_flats.html' , function (){ 
+					$(".popup_caller").magnificPopup({
+						type : 'inline',
+						preloader : false,
+						removalDelay : 300,
+						mainClass : 'my-mfp-zoom-in',
+						overflowY : "scroll",
+	                    close: function() {
+	                        $("#response_popup").empty();
+	                    }
+					});
+					$(".popup_caller").click();
+				} );
+				
+				//load потому что мы не на сервере, при переносе на сервак лучше бы заменить на это
+				/*$.get('get_flats.php [или какой угодно удобный путь к скрипту, важно лишь то, что он вернет]', $(this).data(), function(innerHTML){
+					$("#response_popup").html(innerHTML);
+					$(".popup_caller").magnificPopup({
+						type : 'inline',
+						preloader : false,
+						removalDelay : 300,
+						mainClass : 'my-mfp-zoom-in',
+						overflowY : "scroll",
+	                    close: function() {
+	                        $("#response_popup").empty();
+	                    }
+					});
+					$(".popup_caller").click();
+				});*/
+			});
+
+		}
+
+	};
+
+}); 
